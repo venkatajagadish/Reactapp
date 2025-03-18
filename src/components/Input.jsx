a1806a932bf70bcc86705ad01ce4b41e6874ee89
venkatajagadish <jagadishreddy445@gmail.com>
2025-03-18 13:49:26 +0530

    made the input fields editable

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,18 @@
-export default function Input({ children, inputValue }) {
+import { useState } from "react";
+export default function Input({ children, inputValue, inputType }) {
+  const [currentValue, setCurrentValue] = useState(inputValue);
+  function inputChangeHandler(event) {
+    setCurrentValue(event.target.value);
+  }
   return (
     <span id="user-input">
       <label htmlFor="investment-input">{children}</label>
-      <input id="investment-input" type="number" value={inputValue} />
+      <input
+        id="investment-input"
+        type="number"
+        value={currentValue}
+        onChange={inputChangeHandler}
+      />
     </span>
   );
 }
