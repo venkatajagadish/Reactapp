386416d249b3d12c57dce16b6aaab4f02719e828
venkatajagadish <jagadishreddy445@gmail.com>
2025-03-18 14:14:56 +0530

    reduced the repetition of Input component using map function in JSX

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -4,10 +4,10 @@ import { useState } from "react";
 
 export default function InputGroup() {
   const [inputState, setInputState] = useState([
-    { inputField: "initial", value: 15000, inputLabel: "Initial Investment" },
-    { inputField: "annual", value: 900, inputLabel: "Annual Investment" },
-    { inputField: "expected", value: 5.56, inputLabel: "Expected Return" },
-    { inputField: "duration", value: 10, inputLabel: "Duration(Years)" },
+    { inputType: "initial", value: 15000, inputLabel: "Initial Investment" },
+    { inputType: "annual", value: 900, inputLabel: "Annual Investment" },
+    { inputType: "expected", value: 5.56, inputLabel: "Expected Return" },
+    { inputType: "duration", value: 10, inputLabel: "Duration(Years)" },
   ]);
   function InputStateHandler(inputType, inputValue) {
     setInputState((prevState) => {
@@ -17,7 +17,7 @@ export default function InputGroup() {
         }),
       ];
       return stateCopy.map((input) => {
-        if (input.inputField === inputType) {
+        if (input.inputType === inputType) {
           input.value = inputValue;
         }
         return input;
@@ -29,34 +29,18 @@ export default function InputGroup() {
       <h1>Investment Calculator</h1>
       <img src={appImg} alt="moneyLogo" />
       <div className="input-group">
-        <Input
-          inputType={inputState[0].inputField}
-          inputValue={inputState[0].value}
-          inputStateHandler={InputStateHandler}
-        >
-          {inputState[0].inputLabel}
-        </Input>
-        <Input
-          inputType={inputState[1].inputField}
-          inputValue={inputState[1].value}
-          inputStateHandler={InputStateHandler}
-        >
-          {inputState[1].inputLabel}
-        </Input>
-        <Input
-          inputType={inputState[2].inputField}
-          inputValue={inputState[2].value}
-          inputStateHandler={InputStateHandler}
-        >
-          {inputState[2].inputLabel}
-        </Input>
-        <Input
-          inputType={inputState[3].inputField}
-          inputValue={inputState[3].value}
-          inputStateHandler={InputStateHandler}
-        >
-          {inputState[3].inputLabel}
-        </Input>
+        {inputState.map((input) => {
+          return (
+            <Input
+              key={input.inputType}
+              inputType={input.inputType}
+              inputValue={input.value}
+              inputStateHandler={InputStateHandler}
+            >
+              {input.inputLabel}
+            </Input>
+          );
+        })}
       </div>
       <button>Calculate</button>
     </div>
